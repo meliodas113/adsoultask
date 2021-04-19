575441b3885cf63fffc43cd1ba763f6ed5fc8e32
meliodas113 <f20180433@pilani.bits-pilani.ac.in>
2021-04-20 02:15:56 +0530

    Updates

--- src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-
-import cardSlice from "./card-slice";
-
-const store = configureStore({
-  reducer: { card: cardSlice.reducer }
-});
-
-export default store;
